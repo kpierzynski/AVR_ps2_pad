27a1a7bb16783973934844890f42b671cede6dbd
kpierzynski <kpierzynski@hotmail.com>
2023-02-20 00:59:06 +0100

    Add draft working, not clean solution

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
+#include <string.h>
 
 #include "spi.h"
 #include "uart.h"
@@ -32,10 +33,50 @@ typedef struct
 uint8_t send(uint8_t byte)
 {
 	register uint8_t value;
-
 	value = spi(byte);
-
 	return value;
+}
+
+void send_packet(uint8_t *packet, uint8_t len, uint8_t *result)
+{
+	PORT_SS &= ~(1 << SS);
+
+	while (len--)
+		*result++ = send(*packet++);
+
+	PORT_SS |= (1 << SS);
+}
+
+uint8_t startup[] = {0x01, 0x42, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+uint8_t config[] = {0x01, 0x43, 0x00, 0x01, 0x00};
+uint8_t end_config[] = {0x01, 0x43, 0x00, 0x00, 0x5A, 0x5A, 0x5A, 0x5A, 0x5A};
+uint8_t type[] = {0x01, 0x45, 0x00, 0x5A, 0x5A, 0x5A, 0x5A, 0x5A, 0x5A};
+uint8_t read[] = {0x01, 0x42, 0x00, 0x00, 0x00};
+uint8_t mode[] = {0x01, 0x44, 0x00, 0x01, 0x03, 0x00, 0x00, 0x00, 0x00};
+uint8_t large[] = {0x01, 0x4F, 0x00, 0xFF, 0xFF, 0x03, 0x00, 0x00, 0x00};
+uint8_t answer[32];
+
+void read_pad(ps2_pad_t *pad)
+{
+	uint8_t *packet = read;
+	uint8_t len = 5;
+
+	PORT_SS &= ~(1 << SS);
+
+	while (len--)
+		send(*packet++);
+
+	uint8_t *raw_pad = (uint8_t *)pad;
+	for (uint8_t i = 0; i < sizeof(ps2_pad_t); i++)
+	{
+		*raw_pad++ = send(0x00);
+	}
+	PORT_SS |= (1 << SS);
+}
+
+void print_pad(ps2_pad_t *pad)
+{
+	uart_putbuf((uint8_t *)pad, sizeof(ps2_pad_t), "PAD");
 }
 
 int main()
@@ -48,44 +89,50 @@ int main()
 
 	_delay_ms(1000);
 	ps2_pad_t pad;
-	PORT_SS &= ~(1 << SS);
-	uint8_t header_1 = send(0x01);
-	uint8_t header_2 = send(0x42);
-	uint8_t header_3 = send(0x00);
 
-	if (!(header_1 == 0xFF && header_2 == 0x79 && header_3 == 0x5A))
-	{
-		// not ok
-	}
+	send_packet(startup, 9, answer);
+	uart_putbuf(answer, 9, "STARTUP_ANSWER");
 
-	uint8_t buttons_1 = send(0x00);
-	uint8_t buttons_2 = send(0x00);
+	send_packet(config, 5, answer);
+	uart_putbuf(answer, 5, "CONFIG_ANSWER");
 
-	for (uint8_t i = 0; i < sizeof(ps2_pad_t); i++)
-	{
-		*((uint8_t *)(&pad) + i) = send(0x00);
-	}
-	PORT_SS |= (1 << SS);
+	send_packet(type, 9, answer);
+	uart_putbuf(answer, 9, "TYPE_ANSWER");
 
-	uart_puth(header_1);
-	uart_putc(' ');
-	uart_puth(header_2);
-	uart_putc(' ');
-	uart_puth(header_3);
-	uart_putc(' ');
-	uart_puth(buttons_1);
-	uart_putc(' ');
-	uart_puth(buttons_2);
-	uart_putc(' ');
+	send_packet(mode, 9, answer);
+	uart_putbuf(answer, 9, "MODE_ANSWER");
 
-	for (uint8_t i = 0; i < sizeof(ps2_pad_t); i++)
-	{
-		uart_puth(*((uint8_t *)(&pad) + i));
-		uart_putc(' ');
-	}
+	send_packet(large, 9, answer);
+	uart_putbuf(answer, 9, "LARGE_ANSWER");
+
+	send_packet(end_config, 9, answer);
+	uart_putbuf(answer, 9, "END_CONFIG_ANSWER");
 
 	while (1)
 	{
+		_delay_ms(1000);
+		memset(answer, 0, 32);
+		memset(&pad, 0, sizeof(ps2_pad_t));
+		read_pad(&pad);
+		print_pad(&pad);
+
+		uart_puth(pad.rightX);
+		uart_putc(' ');
+
+		uart_puth(pad.rightY);
+		uart_putc(' ');
+
+		uart_puth(pad.leftX);
+		uart_putc(' ');
+
+		uart_puth(pad.leftY);
+		uart_putc(' ');
+
+		uart_puth(pad.r1Button);
+		uart_putc(' ');
+
+		uart_puth(pad.l1Button);
+		uart_putc(' ');
 	}
 
 	return -1;
--- spi.c
+++ spi.c
@@ -12,8 +12,8 @@ void spi_init()
 	PORT_SCK |= (1 << SCK);
 	PORT_MOSI |= (1 << MOSI);
 
-	SPCR = (0 << SPIE) | (1 << SPE) | (0 << DORD) | (1 << MSTR) | (1 << CPOL) | (1 << CPHA) | (0 << SPR1) | (1 << SPR0);
-	SPSR |= (0 << SPI2X);
+	SPCR = (0 << SPIE) | (1 << SPE) | (1 << DORD) | (1 << MSTR) | (1 << CPOL) | (1 << CPHA) | (1 << SPR1) | (1 << SPR0);
+	SPSR = (0 << SPI2X);
 }
 
 uint8_t spi(uint8_t byte)
